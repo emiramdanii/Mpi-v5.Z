537d70988fe9e38db20d26eca60fb55af9223dcb
Z User <z@container>
2026-04-28 02:35:09 +0000

    f0a60b3f-68b1-4cf4-8aaf-00d64bd6a754

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function Home() {
+  return (
+    <iframe
+      src="/index.html"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+      }}
+      title="Authoring Tool v3"
+    />
+  )
+}
